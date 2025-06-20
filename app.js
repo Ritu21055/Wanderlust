9d689ed89ca9d0314c11e66a42f4e45b48ea4a86
Ritu21055 <rutichittimalle3210@gmail.com>
2025-06-20 16:40:45 +0545

    Fix port for Render deployment

--- app.js
+++ app.js
@@ -133,7 +133,11 @@ app.use((err,req,res,next) =>{
     // res.status(statusCode).send(message);
 })
 
-app.listen(8080,()=>{
-    console.log("server is listening on 8080 port");
-})
+// app.listen(8080,()=>{
+//     console.log("server is listening on 8080 port");
+// })
 
+const port = process.env.PORT || 8080;
+app.listen(port,()=>{
+    console.log(`server is listening on port ${port}`);
+})
